f87a634bbe7c4f1288b8cbdbcbe1676e78babe8a
louislam <louislam7229@gmail.com>
2023-06-08 16:35:11 -0700

    remove unecessary imports

--- payroll-pal-extension/src/App.tsx
+++ payroll-pal-extension/src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import TimeCard from './TimeCard'
 
@@ -8,20 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Payroll Pal</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     <TimeCard/>
+      </header>
     </div>
   );
 }
