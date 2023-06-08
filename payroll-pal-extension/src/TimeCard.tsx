e9ba7826cbc2cc1f58a600b5512309cf57b8e0f4
louislam <louislam7229@gmail.com>
2023-06-08 16:03:46 -0700

    create timecard list

---
+++ payroll-pal-extension/src/TimeCard.tsx
@@ -0,0 +1,18 @@
+
+const TimeCard = () => {
+  return (
+    <div>
+      <ul>
+        <li>MON</li>
+        <li>TUE</li>
+        <li>WED</li>
+        <li>THUR</li>
+        <li>FRI</li>
+        <li>SAT</li>
+        <li>SUN</li>
+      </ul>
+    </div>
+    )
+}
+
+export default TimeCard;
