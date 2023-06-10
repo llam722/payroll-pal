f35cd8c3a1777ec4ebf231094ee88eb0123d777d
louislam <louislam7229@gmail.com>
2023-06-10 16:13:11 -0700

    create TimeStamp.tsx

--- payroll-pal-extension/src/TimeCard.tsx
+++ payroll-pal-extension/src/TimeCard.tsx
@@ -4,6 +4,7 @@ const TimeCard = () => {
   return (
     <div className="timecard">
 
+      <header className="timecard-header">
       <div className="timecard-in">
       <h2>IN</h2>
       </div>
@@ -11,25 +12,19 @@ const TimeCard = () => {
       <div className="timecard-out">
       <h2>OUT</h2>
       </div>
+
+      </header>
          
-      {/* <section className="timecard-body">
-        <ul>
-          <li><h5>MON</h5></li>
-          <li><h5>TUE</h5></li>
-          <li><h5>WED</h5></li>
-          <li><h5>THU</h5></li>
-          <li><h5>FRI</h5></li>
-          <li><h5>SAT</h5></li>
-          <li><h5>SUN</h5></li>
-        </ul>
-      </section> */}
-        
+      
+        <div className="calculations">
         <div className="hours">
           <h4>Hours</h4>
         </div>
-        
+
         <div className="hours-in-decimal">
           <h4>Decimal</h4>
+
+        </div>
         </div>
 
 
---
+++ payroll-pal-extension/src/TimeStamps.tsx
@@ -0,0 +1,5 @@
+const TimeStamps () => {
+  return (
+    
+  )
+}
