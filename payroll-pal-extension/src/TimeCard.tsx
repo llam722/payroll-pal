48b0342504089d67d4eb20606a36bbfe729ed097
louislam <louislam7229@gmail.com>
2023-06-10 15:59:44 -0700

    create Hours and Decimals column

--- payroll-pal-extension/src/TimeCard.tsx
+++ payroll-pal-extension/src/TimeCard.tsx
@@ -3,11 +3,16 @@ import'./TimeCard.css'
 const TimeCard = () => {
   return (
     <div className="timecard">
-      <header className="timecard-header">
+
+      <div className="timecard-in">
       <h2>IN</h2>
+      </div>
+
+      <div className="timecard-out">
       <h2>OUT</h2>
-      </header>
-      <section className="timecard-body">
+      </div>
+         
+      {/* <section className="timecard-body">
         <ul>
           <li><h5>MON</h5></li>
           <li><h5>TUE</h5></li>
@@ -17,8 +22,17 @@ const TimeCard = () => {
           <li><h5>SAT</h5></li>
           <li><h5>SUN</h5></li>
         </ul>
-      </section>
-    <div className="hours"></div>
+      </section> */}
+        
+        <div className="hours">
+          <h4>Hours</h4>
+        </div>
+        
+        <div className="hours-in-decimal">
+          <h4>Decimal</h4>
+        </div>
+
+
   </div>
   )
 };
