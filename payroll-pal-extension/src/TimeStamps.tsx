32b14ebaf73bb58d97a5227381a2f5b9f445de9d
louislam <louislam7229@gmail.com>
2023-06-10 16:13:45 -0700

    create section and list for timestamps

--- payroll-pal-extension/src/TimeStamps.tsx
+++ payroll-pal-extension/src/TimeStamps.tsx
@@ -1,5 +1,17 @@
-const TimeStamps () => {
+const TimeStamps = () => {
   return (
-    
+      <section className="timecard-body">
+        <ul>
+          <li><h5>MON</h5></li>
+          <li><h5>TUE</h5></li>
+          <li><h5>WED</h5></li>
+          <li><h5>THU</h5></li>
+          <li><h5>FRI</h5></li>
+          <li><h5>SAT</h5></li>
+          <li><h5>SUN</h5></li>
+        </ul>
+      </section>
   )
 }
+
+export default TimeStamps;
