63c068f98e97a209bf43f5a59609821978a20855
louislam <louislam7229@gmail.com>
2023-06-10 22:43:28 -0700

    containerize left side of timestamps;

--- payroll-pal-extension/src/TimeCard.tsx
+++ payroll-pal-extension/src/TimeCard.tsx
@@ -1,13 +1,18 @@
 import "./TimeCard.css";
+import Days from './Days'
 
 const TimeCard = () => {
   return (
     <div className="timecard">
       <div className="timecard-header">
-
-        <h2>IN</h2>
+        <div className="timecard-in-out">
+          <h2>IN</h2>
         <h2>OUT</h2>
-
+        </div>
+        
+        <div className="timestamps">
+          <p>inout</p>
+      </div>
       </div>
 
       <div className="calculations">
