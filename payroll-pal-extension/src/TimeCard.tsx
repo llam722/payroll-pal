5546fa5d9baf38c4b917aa3406bf0afba3451c31
louislam <louislam7229@gmail.com>
2023-06-10 17:58:37 -0700

    add flex grow properties

--- payroll-pal-extension/src/TimeCard.tsx
+++ payroll-pal-extension/src/TimeCard.tsx
@@ -3,16 +3,16 @@ import "./TimeCard.css";
 const TimeCard = () => {
   return (
     <div className="timecard">
-      <h2>IN</h2>
+      <div className="timecard-header">
+        <h2>IN</h2>
 
-      <h2>OUT</h2>
+        <h2>OUT</h2>
+      </div>
+
       <div className="calculations">
-        
-      <h4>Hours</h4>
-      <h4>Decimal</h4>
-
-    </div>
-
+        <h4>Hours</h4>
+        <h4>Decimal</h4>
+      </div>
     </div>
   );
 };
