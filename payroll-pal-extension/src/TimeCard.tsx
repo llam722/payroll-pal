b4ed4c317d5d5e179e7c35be7408d787460c4656
louislam <louislam7229@gmail.com>
2023-06-10 18:09:46 -0700

    add hours and decimal background

--- payroll-pal-extension/src/TimeCard.tsx
+++ payroll-pal-extension/src/TimeCard.tsx
@@ -5,15 +5,14 @@ const TimeCard = () => {
     <div className="timecard">
       <div className="timecard-header">
 
-
         <h2>IN</h2>
         <h2>OUT</h2>
-      
+
       </div>
 
       <div className="calculations">
-        <h4>Hours</h4>
-        <h4>Decimal</h4>
+        <h4 className="hours">Hours</h4>
+        <h4 className="hours">Decimal</h4>
       </div>
     </div>
   );
